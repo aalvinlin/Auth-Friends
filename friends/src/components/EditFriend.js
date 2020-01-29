a81c7c7e14e85f61ec719e35952a1fa79cedf2b8
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-01-28 18:02:40 -0800

    edit friends almost working. need to make a <select> change all initial inputs upon onChange

--- friends/src/components/EditFriend.js
+++ friends/src/components/EditFriend.js
@@ -1,7 +1,34 @@
-import React, { useState, useHistory } from "react";
+import React, { useState, useEffect } from "react";
 import {axiosWithAuth} from "../utils/axiosWithAuth";
+import e from "express";
 
-const AddFriend = (props) => {
+const EditFriend = (props) => {
+
+    const [existingFriends, setExistingFriends] = useState(undefined);
+    const [isLoading, setIsLoading] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(undefined);
+
+    const [friendData, setFriendData] = useState(initialFriendData);
+
+    useEffect(() => {
+
+        axiosWithAuth()
+            .get("friends")
+            .then(response => {
+                console.log("friends retrieved from server:", response);
+                setExistingFriends(response.data);
+
+                if (response.data.length > 0)
+                    { setFriendData(response.data[0]); }
+                    
+                setIsLoading(false);
+            })
+            .catch(error => {
+                console.log("error from server:", error);
+                setErrorMessage("Could not access server.")
+            })
+
+    }, []);
 
     const initialFriendData = {
         id: 0,
@@ -10,11 +37,15 @@ const AddFriend = (props) => {
         email: ""
     }
 
-    const [friendData, setFriendData] = useState(initialFriendData);
-    const [errorMessage, setErrorMessage] = useState("");
-
     const handleChange = (event) => {
-        setFriendData({...friendData, [event.target.name]: event.target.value });
+
+        // if the dropdown menu is changed to a different option, change all input fields.
+        if (event.target.type === "select")
+            {
+                setFriendData({...friendData, [event.target.name]: event.target.value });
+            }
+        else
+            { setFriendData({...friendData, [event.target.name]: event.target.value }); }
     }
 
     const handleSubmit = (event) => {
@@ -22,7 +53,7 @@ const AddFriend = (props) => {
 
         let friendId = Date.now();
 
-        console.log("trying to add friend with", friendData.name, friendData.age, friendData.email, friendId);
+        console.log("trying to edit friend with", friendData.name, friendData.age, friendData.email, friendId);
 
         axiosWithAuth()
             .post("friends/", friendData)
@@ -39,11 +70,19 @@ const AddFriend = (props) => {
             })
     }
 
+    console.log(friendData, "is friendData");
+    console.log(existingFriends, "is friends");
+
+    if (!friendData)
+        { return <h2>Loading friend data...</h2>}
+
     return (
         <form name="addFriend" className="addFriendForm">
 
             <label htmlFor="name">Name:
-                <input type="text" name="name" value={friendData.name} onChange={handleChange}  className="dividerColor1"/>
+                <select name="name" value={friendData.name} onChange={handleChange}  className="dividerColor1">
+                    { existingFriends.map(friend => <option value={friend.name}>{friend.name}</option>) }
+                </select>
             </label>
 
             <label htmlFor="age">Age:
@@ -54,7 +93,7 @@ const AddFriend = (props) => {
                 <input type="text" name="email" value={friendData.email} onChange={handleChange}  className="dividerColor3"/>
             </label>
 
-            <button name="submit" onClick={handleSubmit}>&#9679; Add Friend &#9679;</button>
+            <button name="submit" onClick={handleSubmit}>&#9679; Edit Friend &#9679;</button>
 
             <p className="formError">{errorMessage}</p>
         </form>
@@ -62,4 +101,4 @@ const AddFriend = (props) => {
 
 }
 
-export default AddFriend;
+export default EditFriend;
